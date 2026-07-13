39dcbecca99fbcf8ddb7d5d24fbd211a179086ce
Sruthi <msruthi251999@gmail.com>
2026-07-13 13:48:24 -0400

    Update SECRET_KEY fallback to use dynamic os.urandom

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, "instance", "BlogDB.db")
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-for-local-development')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', os.urandom(24))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_PATH}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
